53a3a15e4c56c220762351ef66ff52413c94f0a5
Yan <ydamelio@outlook.de>
2022-08-03 14:04:02 +0200

    changed all files from HTML to PHP

---
+++ Html/account.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accountt</title>
+</head>
+
+<body>
+    <nav>
+        <ul
+          style="
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+          "
+        >
+          <li><a href="index.html">Home</a></li>
+          <li><a href="Artist.html">Artist</a></li>
+          <li><a href="songs.html">Song</a></li>
+          <li><a href="playlist.html">Playlist</a></li>
+          <li><a href="register.html">Register/Login</a></li>
+          <li><a href="account.html">Account</a></li>
+        </ul>
+      </nav>
+</body>
+
+</html>
--- Html/register.php
+++ Html/register.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SONGIFY - Index</title>
+    <title>Register</title>
   </head>
+
   <body>
     <nav>
       <ul
@@ -23,14 +24,5 @@
         <li><a href="account.html">Account</a></li>
       </ul>
     </nav>
-    <div>
-      <h1><i>Songify is your Music Page</i></h1>
-      <p>Welcome to Songify!</p>
-      <p>We have the best Songs from the best <i>Artists</i></p>
-      <p>
-        Dont forget to <i><a href="register.html">register</a></i> first before
-        creating your Playlists.
-      </p>
-    </div>
   </body>
 </html>
