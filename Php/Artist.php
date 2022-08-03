d25322543263ca67bff0de6c9b53c6f187616680
SonGoken <fabio27nevpr@gmail.com>
2022-08-03 14:38:09 +0200

    hello works plase

--- Php/Artist.php
+++ Php/Artist.php
@@ -2,19 +2,16 @@
 
 
 $conn = mysqli_connect('localhost', 'root', '', 'spotify_db');
-
-// True if connected, false if not
 if ($conn) {
     echo 'Connected successfully<br>';
 
-    // 2. Prepare the query
+
     $query = 'SELECT * FROM artists';
 
-    // 3. Ask DB to run/execute the query 
+
     $results = mysqli_query($conn, $query);
 
-    // I retrieved my results but I need to fetch before using them
-    // 4. Fetch the results as an associative array
+
     $artists = mysqli_fetch_all($results, MYSQLI_ASSOC);
 } else {
     echo 'Problem connecting with the database';
@@ -39,6 +36,21 @@ if ($conn) {
 
 
 <body>
+
+    <nav>
+        <ul style="
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        ">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="Artist.html">Artist</a></li>
+            <li><a href="songs.html">Song</a></li>
+            <li><a href="playlist.html">Playlist</a></li>
+            <li><a href="register.html">Register/Login</a></li>
+            <li><a href="account.html">Account</a></li>
+        </ul>
+    </nav>
     <?php foreach ($artists as $artist) : ?>
 
         <p>
