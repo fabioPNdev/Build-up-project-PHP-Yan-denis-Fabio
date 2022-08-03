ba091fb735d3ada871b092b05a2593aabefd7d77
Yan <ydamelio@outlook.de>
2022-08-03 14:12:30 +0200

    updated index file

--- Html/index.php
+++ Html/index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SONGIFY - Index</title>
-  </head>
 
-  <body>
-    <nav>
-      <ul
-        style="
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>SONGIFY - Index</title>
+</head>
+
+<body>
+  <nav>
+    <ul style="
           display: flex;
           justify-content: space-around;
           align-items: center;
-        "
-      >
-        <li><a href="index.html">Home</a></li>
-        <li><a href="Artist.html">Artist</a></li>
-        <li><a href="songs.html">Song</a></li>
-        <li><a href="playlist.html">Playlist</a></li>
-        <li><a href="register.html">Register/Login</a></li>
-        <li><a href="account.html">Account</a></li>
-      </ul>
-    </nav>
-    <div>
-      <h1><i>Songify is your Music Page</i></h1>
-      <p>Welcome to Songify!</p>
-      <p>We have the best Songs from the best <i>Artists</i></p>
-      <p>
-        Dont forget to <i><a href="register.html">register</a></i> first before
-        creating your Playlists.
-      </p>
-    </div>
-  </body>
+        ">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="Artist.php">Artist</a></li>
+      <li><a href="songs.php">Song</a></li>
+      <li><a href="playlist.php">Playlist</a></li>
+      <li><a href="register.php">Register/Login</a></li>
+      <li><a href="account.php">Account</a></li>
+    </ul>
+  </nav>
+  <div>
+    <h1><i>Songify is your Music Page</i></h1>
+    <p>Welcome to Songify!</p>
+    <p>We have the best Songs from the best <i>Artists</i></p>
+    <p>
+      Dont forget to <i><a href="register.php">register</a></i> first before
+      creating your Playlists.
+    </p>
+  </div>
+</body>
+
 </html>
